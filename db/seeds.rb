c96e64ae690969e36a150a3d3b3619be1de8201f
Francesca Hall <francescahall88@gmail.com>
2020-08-12 15:05:59 -0400

    the usual mistakes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+spazz_house = { name: 'Spazz House', address: 'Verdun', category: 'japanese' }
+clochette = { name: 'Clochette', address: 'Brighton', category: 'french' }
+nona = { name: 'Nona', address: 'Little Italy', category: 'italian' }
+frits_alors = { name: 'Frits Alors', address: 'Plateau', category: 'french' }
+city_wok = { name: 'City Wok', address: 'China Town', category: 'chinese' }
+
+[spazz_house, clochette, nona, frits_alors, city_wok].each do |attributes|
+  Restaurant.create!(attributes)
+end
