9466db69d5e5fcd128b5430a0a572ed5d97fa67f
Francesca Hall <francescahall88@gmail.com>
2020-08-12 16:34:58 -0400

    85% complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,26 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    new_restaurant = Restaurant.new(restaurant_params)
+    new_restaurant.save
+
+    redirect_to restaurant_path(new_restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
